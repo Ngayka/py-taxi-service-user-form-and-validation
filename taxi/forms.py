5b16ad0c0e57aaf3400f075c10c370eac24b8101
Ngayka <ngayka@gmail.com>
2025-05-12 20:11:39 +0300

    error if fixed

--- taxi/forms.py
+++ taxi/forms.py
@@ -18,7 +18,7 @@ class DriverLicenseUpdateForm(forms.ModelForm):
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
         if len(license_number) != 8:
-            raise forms.ValidationError("Liсense number must be 8 symbols")
+            raise forms.ValidationError("License number must be 8 symbols")
         elif not license_number[0:2].isalpha() or not license_number[2::].isdigit():
             raise forms.ValidationError("Please enter a valid number")
         return license_number
