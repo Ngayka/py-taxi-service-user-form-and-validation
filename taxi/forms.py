95fc29ff1df891028c46d7fe3983cf4ae5f0f796
Ngayka <ngayka@gmail.com>
2025-05-12 19:53:33 +0300

    changed indents, errors and missed trailing slash

--- taxi/forms.py
+++ taxi/forms.py
@@ -9,17 +9,17 @@ class DriverForm(forms.ModelForm):
         model = Driver
         fields = "__all__"
 
+
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
         model = Driver
         fields = ("license_number",)
 
-
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
-        if len(license_number) != 8 :
-            raise forms.ValidationError("Lisense number must be 8 symbols")
-        elif not license_number[0:2].isalpha() or not license_number[2::].isdigit() :
+        if len(license_number) != 8:
+            raise forms.ValidationError("Liсense number must be 8 symbols")
+        elif not license_number[0:2].isalpha() or not license_number[2::].isdigit():
             raise forms.ValidationError("Please enter a valid number")
         return license_number
 
